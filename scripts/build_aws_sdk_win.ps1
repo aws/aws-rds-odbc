0c8126d721ea198bd02b32609b319b275162d18e
Karen Chen <karenc@bitquilltech.com>
2025-02-24 11:25:12 -0800

    chore: change to Apache 2 license
    # Conflicts:
    #	scripts/build_aws_sdk_linux.sh
    #	scripts/build_aws_sdk_macos
    #	scripts/build_aws_sdk_win.ps1
    
    Update readme

--- scripts/build_aws_sdk_win.ps1
+++ scripts/build_aws_sdk_win.ps1
@@ -1,32 +1,17 @@
 <#
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2.0
-(GPLv2), as published by the Free Software Foundation, with the
-following additional permissions:
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is distributed with certain software that is licensed
-under separate terms, as designated in a particular file or component
-or in the license documentation. Without limiting your rights under
-the GPLv2, the authors of this program hereby grant you an additional
-permission to link the program and your derivative works with the
-separately licensed software that they have included with the program.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-Without limiting the foregoing grant of rights under the GPLv2 and
-additional permission as to separately licensed software, this
-program is also subject to the Universal FOSS Exception, version 1.0,
-a copy of which can be found along with its FAQ at
-http://oss.oracle.com/licenses/universal-foss-exception.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License, version 2.0, for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see 
-http://www.gnu.org/licenses/gpl-2.0.html.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 /#>
 
 $CURRENT_DIR = (Get-Location).Path
