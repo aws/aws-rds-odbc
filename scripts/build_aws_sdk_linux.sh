24dda510812439c23ea90d2a842ecb7581a6f789
Karen <64801825+karenc-bq@users.noreply.github.com>
2025-02-24 10:36:55 -0800

    chore: fix license headers (#42)

--- scripts/build_aws_sdk_linux.sh
+++ scripts/build_aws_sdk_linux.sh
@@ -1,5 +1,21 @@
 
-#Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# //
+# // This library is free software; you can redistribute it and/or
+# // modify it under the terms of the GNU Library General Public
+# // License as published by the Free Software Foundation; either
+# // version 2 of the License, or (at your option) any later version.
+# //
+# // This library is distributed in the hope that it will be useful,
+# // but WITHOUT ANY WARRANTY; without even the implied warranty of
+# // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# // Library General Public License for more details.
+# //
+# // You should have received a copy of the GNU Library General Public
+# // License along with this library; if not, write to the Free Software
+# // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
 
 #This program is free software; you can redistribute it and/or modify
 #it under the terms of the GNU General Public License, version 2.0
